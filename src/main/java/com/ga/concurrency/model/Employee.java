318f4ce7de8397c6c845ad84d1f3ee166b1bd3ae
nadia-husain <nadia.neerooz@gmail.com>
2026-03-30 13:24:41 +0300

    added yearBonus and roleBonus and added json property order

--- src/main/java/com/ga/concurrency/model/Employee.java
+++ src/main/java/com/ga/concurrency/model/Employee.java
@@ -1,5 +1,6 @@
 package com.ga.concurrency.model;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.ga.concurrency.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,17 @@ import java.time.LocalDate;
 
 @Setter
 @Getter
+@JsonPropertyOrder({
+        "id",
+        "name",
+        "role",
+        "joiningDate",
+        "projectCompletion",
+        "salary",
+        "roleBonus",
+        "yearBonus",
+        "updatedSalary"
+})
 public class Employee {
     private int id;
     private String name;
@@ -15,15 +27,19 @@ public class Employee {
     private LocalDate joiningDate;
     private Role role;
     private double projectCompletion;
+    private double roleBonus;
+    private double yearBonus;
     private double updatedSalary;
 
-    public Employee(int id, String name, double salary, LocalDate joiningDate, Role role, double projectCompletion, double updatedSalary) {
+    public Employee(int id, String name, double salary, LocalDate joiningDate, Role role, double projectCompletion, double roleBonus, double yearBonus, double updatedSalary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
         this.joiningDate = joiningDate;
         this.role = role;
         this.projectCompletion = projectCompletion;
+        this.roleBonus = roleBonus;
+        this.yearBonus = yearBonus;
         this.updatedSalary = updatedSalary;
     }
 
