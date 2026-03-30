b0f69f6d5ade652a2f2008516664440b5ae1c9f5
nadia-husain <nadia.neerooz@gmail.com>
2026-03-30 12:42:37 +0300

    updated controller

--- src/main/java/com/ga/concurrency/controller/ConcurrencyController.java
+++ src/main/java/com/ga/concurrency/controller/ConcurrencyController.java
@@ -1,10 +1,15 @@
 package com.ga.concurrency.controller;
 
+import com.ga.concurrency.service.ConcurrencyService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/concurrency")
 public class ConcurrencyController {
+    private final ConcurrencyService concurrencyService;
 
+    public ConcurrencyController(ConcurrencyService concurrencyService) {
+        this.concurrencyService = concurrencyService;
+    }
 }
